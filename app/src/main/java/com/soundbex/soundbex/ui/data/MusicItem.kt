8f333520733864fd486818f60d335f7a15cdd541
Ekin Ilter Varli <ekinnos@tutanota.com>
2025-10-18 00:36:05 +0300

    Remove files from repo but keep locally

--- app/src/main/java/com/soundbex/soundbex/ui/data/MusicItem.kt
+++
@@ -1,7 +0,0 @@
-package com.soundbex.soundbex.data
-
-data class MusicItem(
-    val title: String,
-    val artist: String,
-    val coverUrl: String
-)
--- app/src/main/java/com/soundbex/soundbex/ui/data/PodcastItem.kt
+++
@@ -1,7 +0,0 @@
-package com.soundbex.soundbex.data
-
-data class PodcastItem(
-    val title: String,
-    val host: String,
-    val coverUrl: String
-)
